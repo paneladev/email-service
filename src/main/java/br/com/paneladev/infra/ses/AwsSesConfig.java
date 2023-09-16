1ddd6e11b6ca5bc3333caa128bb8c4b2f94372fd
paneladev <paneladev@gmail.com>
2023-09-16 13:39:50 -0300

    ajuste para passar as credenciais na configuração do ses

--- src/main/java/br/com/paneladev/infra/ses/AwsSesConfig.java
+++ src/main/java/br/com/paneladev/infra/ses/AwsSesConfig.java
@@ -1,16 +1,31 @@
 package br.com.paneladev.infra.ses;
 
-import com.amazonaws.regions.Regions;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AwsSesConfig {
 
+    @Value("${access-key}")
+    private String accessKey;
+
+    @Value("${secret-key}")
+    private String secretKey;
+
+    @Value("${region}")
+    private String region;
+
     @Bean
     public AmazonSimpleEmailService amazonSimpleEmailService() {
-        return AmazonSimpleEmailServiceClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonSimpleEmailServiceClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
     }
 }
